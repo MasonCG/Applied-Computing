6857d78b93ec2a89013b6013c757e531c78c4438
Mason Goddard <107564580+MasonCG@users.noreply.github.com>
2025-09-05 23:54:12 -0400

    Finished bonus and fixed Askii Table

--- CollatzTesting.py
+++ CollatzTesting.py
@@ -5,30 +5,37 @@ import Collatz_Optimized as Optimized
 import Collatz_Optimized_Bonus as Bonus
 import time
 
-range = 1_000_000
+
+def testing(range):
+
+    # Setting up Askii Table
+    print(f"Collatz Optimization Testing (1, {range})")
+    print("Collatz Program\t\t Runtime\t\t\t Largest Pair (num, steps)")
+
+    # getting time duration for base Collatz
+    s_time = time.time()
+
+    longest = Base.collatz_length_in_range(1, range)
+
+    duration = time.time() - s_time
+    print("Base:\t\t\t", duration, "s\t\t", longest)
 
 
-# getting time duration for base Collatz
-s_time = time.time()
+    # getting time duration for Optimized Collatz
+    s_time = time.time()
 
-longest = Base.collatz_length_in_range(1, range)
+    longest = Optimized.collatz_length_in_range(1, range)
 
-duration = time.time() - s_time
-print("base: ", duration, "s")
+    duration = time.time() - s_time
+    print("Optimized:\t\t", duration, "s\t\t", longest)
 
-# getting time duration for Optimized Collatz
-s_time = time.time()
+    # getting time duration for Bonus Optimized Collatz
+    s_time = time.time()
 
-longest = Optimized.collatz_length_in_range(1, range)
+    longest = Bonus.collatz_length_in_range(1, range)
 
-duration = time.time() - s_time
-print("base: ", duration, "s")
-
-# getting time duration for Bonus Optimized Collatz
-s_time = time.time()
-
-longest = Bonus.collatz_length_in_range(1, range)
-
-duration = time.time() - s_time
-print("base: ", duration, "s")
-
+    duration = time.time() - s_time
+    print("Bonus:\t\t\t", duration, "s\t\t", longest)
+    print('\n')
+testing(10_000)
+testing(1_000_000)
--- Collatz_Optimized_Bonus.py
+++ Collatz_Optimized_Bonus.py
@@ -5,57 +5,34 @@
 # empty dictionary to store collatz numbers
 collatz_dict = {}
 
-def store_collatz(num):
-    """ Used to store number into a dictionary"""
-    # keeps track of number of steps
-    steps = 1
-    # storing original num
-    n = num
-
-    while (n != 1):
-        
-        if n in collatz_dict:
-            steps += collatz_dict[n] - 1
-            collatz_dict[num] = steps
-            return
-
-        #if num is odd
-        if n % 2:
-            n = n * 3 + 1
-        else:
-            n = n//2
-
-        steps += 1
-    
-    collatz_dict[num] = steps
-
-
-
 def collatz_length(num):
     """Get the number of steps it takes to go through Collatz Conjecture Sequence"""
 
-    # keeps track of number of steps
-    steps = 1
-    # storing original num
+    steps = 0
     n = num
 
-
-
-    while (n != 1):
+    while(n != 1):
         
         if n in collatz_dict:
-            steps += collatz_dict[n] - 1
+            steps += collatz_dict[n]
+            collatz_dict[num] = steps
             return steps
-        else: 
-            store_collatz(n)
-            if n % 2:
-                n = n * 3 + 1
-            else: 
-                n = n//2
+
+        if n % 2:
+            n = n * 3 + 1
+        else:
+            n = n // 2
+
+        if n not in collatz_dict:
+            collatz_length(n)
 
         steps += 1
-        
+
+    steps += 1
+    collatz_dict[num] = steps
     return steps
+    
+
 
 def collatz_length_in_range(s_num, e_num):
     """ Getting the largest number of steps in Collatz Conjecture for each number withing the given range"""
